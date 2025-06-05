02ec0cefc4f4b60ce2cc29ff568e61b43524bdf5
hung9605 <hung9605@gmail.com>
2025-06-05 09:40:40 +0000

    u

---
+++ src/main/java/com/app/customermanagement/config/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.app.customermanagement.config;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+	
+	@KafkaListener(topics = "register-topic", groupId = "console-consumer-74131")
+    public void listen(String message) {
+        System.out.println("Received Message: " + message);
+    }
+
+}
