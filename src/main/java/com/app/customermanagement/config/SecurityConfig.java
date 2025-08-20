4c7fdc3472b7a6ec6a311932495afb255744b8e8
nd.tuan <nd.tuan>
2025-08-20 17:16:18 +0700

    u

--- src/main/java/com/app/customermanagement/config/SecurityConfig.java
+++ src/main/java/com/app/customermanagement/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
 	            .requestMatchers(HttpMethod.POST,"/oauth2/exchange-token").permitAll()
 	            .requestMatchers(HttpMethod.POST,"/oauth2/refresh-token").permitAll()
 	            .requestMatchers(HttpMethod.GET,"/upload/**").permitAll()
+	            .requestMatchers("/actuator/**").permitAll()
 	            .anyRequest().authenticated()
 	        )
 	        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
